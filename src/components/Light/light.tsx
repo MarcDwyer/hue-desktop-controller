c76b153d02e7b1102591cae265c7221f42c7bc77
Marc Dwyer <marcdwyer1738@gmail.com>
2019-08-18 13:44:34 -0500

    light component now using hooks & added custom hook useStyles

--- src/components/Light/light.tsx
+++ src/components/Light/light.tsx
@@ -1,149 +1,128 @@
-import React from 'react'
-import { debounce } from 'lodash'
-import { LightType, LightParent } from '../Main/main'
+import React, { useState, useEffect } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+import { LightType } from '../Main/main'
 import { MdModeEdit } from 'react-icons/md'
 import Switch from '@material-ui/core/Switch';
 
 import './light.scss'
-
 
 interface Props {
     light: LightType;
     setLight: Function;
     alterLight: Function;
 }
-interface State {
-    range: number;
-    disabled: boolean;
-    newName: string;
+const checkRgb = (rgb: number[]): boolean => {
+    let count = 0
+    for (let x = 0, len = rgb.length; x < len; x++) {
+        if (rgb[x] > 165) count++
+    }
+    return count >= 2
 }
 
-
-class Light extends React.Component<Props, State> {
-    constructor(props) {
-        super(props);
-        const { light } = this.props
-        this.sendBright = debounce(this.sendBright, 450)
-        this.state = {
-            range: light.state.bri,
-            disabled: true,
-            newName: this.props.light.name
-        }
-    }
-    sendBright = (id: number, brightness: number) => {
-        this.props.alterLight(id, brightness, "brightness")
-    }
-    getStyles = (): Object => {
-        const { light } = this.props
-        let styles = {}
+const useStyles = (light: LightType): Object[] => {
+    const [style, setStyle] = useState<Object>({})
+    let strRgb = light.rgb ? JSON.stringify(light.rgb) : null
+    useEffect(() => {
+        let styles: Object = {}
         if (light.rgb) {
             if (light.state.on) {
                 styles["backgroundColor"] = `rgb(${light.rgb.join(',')})`
-                if (this.checkRgb(light.rgb)) {
+                if (checkRgb(light.rgb)) {
                     styles["color"] = 'black'
                 }
             } else {
                 styles["backgroundColor"] = `#2B2C2D`
             }
         } else {
-            if (light.state.on) {
-                styles["backgroundColor"] = `#BEBEBE`
-            }
+            if (light.state.on) styles["backgroundColor"] = `#BEBEBE`
         }
-        return styles
-    }
-    checkRgb(rgb: number[]): boolean {
-        let count = 0
-        for (let x = 0; x < rgb.length; x++) {
-            if (rgb[x] > 165) count++
-        }
-        return count >= 2
-    }
-    submitName = () => {
-        const { light, alterLight } = this.props
-        if (light.name === this.state.newName) {
-            this.setState({ disabled: true })
-            return
-        }
-        this.setState({ disabled: true }, () => alterLight(light.id, this.state.newName, "newName"))
-    }
-    render() {
-        const { light, setLight } = this.props
-        const { disabled, newName } = this.state
-        return (
-            <div
-                className='light-parent'
-                style={this.getStyles()}
-            >
-                <Switch
-                    checked={light.state.on}
-                    onChange={() => this.props.alterLight(light.id, !light.state.on, "power")}
-                    value="checkedA"
-                    color="primary"
-                    inputProps={{ 'aria-label': 'secondary checkbox' }}
-                />
-                <div
-                    className='text'
-                    style={light.rgb ? { cursor: 'pointer' } : {}}
-                    onClick={(e: any): void => {
-                        setLight(light.id)
-                    }}
-                >
-                    {!disabled && (
-                        <div className={`hideme ${!disabled ? 'disabled' : ''}`}
-                            onClick={(e) => {
-                                e.stopPropagation()
-                                this.submitName()
-                            }}
-                        ></div>
-                    )}
-                    <div className={`name ${!disabled ? 'activated-div' : ''}`}
-                    >
-                        <MdModeEdit
-                            size="24px"
-                            onClick={(e) => {
-                                e.stopPropagation()
-                                if (!disabled) {
-                                    this.submitName()
-                                    return
-                                }
-                                this.setState({ disabled: !disabled })
+        setStyle(styles)
+    }, [strRgb, light.state.on])
+    return [style]
+}
+const Light = (props: Props) => {
+    const { light, setLight } = props
+    const [range, setRange] = useState<number>(light.state.bri)
+    const [disabled, setDisabled] = useState<boolean>(true)
+    const [name, setName] = useState<string>(light.name)
 
-                            }}
-                        />
-                        <form onSubmit={(e) => {
-                            e.preventDefault()
-                            this.submitName()
-                        }}>
-                            <input
-                                value={newName}
-                                type="text"
-                                size={newName.length}
-                                disabled={disabled}
-                                className={!disabled ? "activated-input" : ""}
-                                onClick={(e) => e.stopPropagation()}
-                                onChange={(e) => this.setState({ newName: e.target.value })}
-                            />
-                        </form>
-                    </div>
-                </div>
-                <div className="range-content">
-                    <input
-                        type="range"
-                        min={0}
-                        max={254}
-                        value={this.state.range}
-                        onChange={(e) => {
-                            const bright = parseInt(e.target.value)
-                            this.setState({ range: bright }, () => {
-                                this.sendBright(light.id, this.state.range)
-                            })
+    const [sendBright] = useDebouncedCallback((id: number, brightness: number) => {
+        props.alterLight(id, brightness, "brightness")
+    }, 450)
+
+    const [submitName] = useDebouncedCallback(() => {
+        if (light.name === name) return
+        props.alterLight(light.id, name, "newName")
+    }, 450)
+
+    const [styleObj] = useStyles(light)
+
+    useEffect(() => {
+        sendBright(light.id, range)
+    }, [range])
+
+    useEffect(() => {
+        submitName()
+    }, [name])
+    return (
+        <div
+            className='light-parent'
+            style={styleObj}
+        >
+            <Switch
+                checked={light.state.on}
+                onChange={() => props.alterLight(light.id, !light.state.on, "power")}
+                value="checkedA"
+                color="primary"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+            <div
+                className='text'
+                style={light.rgb ? { cursor: 'pointer' } : {}}
+                onClick={(e: any) => setLight(light.id)}
+            >
+                {!disabled && (
+                    <div className={`hideme ${!disabled ? 'disabled' : ''}`}
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            setDisabled(true)
                         }}
+                    ></div>
+                )}
+                <div className={`name ${!disabled ? 'activated-div' : ''}`}
+                >
+                    <MdModeEdit
+                        size="24px"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            if (!disabled) {
+                                return
+                            }
+                            setDisabled(false)
+                        }}
+                    />
+                    <input
+                        value={name}
+                        type="text"
+                        size={name.length}
+                        disabled={disabled}
+                        className={!disabled ? "activated-input" : ""}
+                        onClick={(e) => e.stopPropagation()}
+                        onChange={(e) => setName(e.target.value)}
                     />
                 </div>
             </div>
-        )
-    }
+            <div className="range-content">
+                <input
+                    type="range"
+                    min={0}
+                    max={254}
+                    value={range}
+                    onChange={(e) => setRange(parseInt(e.target.value))}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Light
